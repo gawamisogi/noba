5b00b661b64864dcd2d841bc631ceba95717cb1d
gawamisogi <119556847+gawamisogi@users.noreply.github.com>
2023-01-02 09:22:43 +0700

    Add files via upload

---
+++ apalah.js
@@ -0,0 +1,13 @@
+function popupku() {
+  var body = document.querySelector("body");      
+  var pWm = document.createElement("a");
+  pWm.setAttribute("href", "https://www.facebook.com/jeky.jaylana.3");
+  pWm.setAttribute("style", "text-decoration: none; color: white; opacity: .5; position: fixed; bottom : 10px; right: 10px");
+  pWm.innerHTML = "Jeky Jaylana";
+  body.appendChild(pWm);
+}
+  pWm.setAttribute("href", "https://instagram.com/whooopsiie_");
+  pWm.setAttribute("style", "text-decoration: none; color: white; opacity: .5; position: fixed; bottom : 10px; left: 10px");
+  pWm.innerHTML = "Ratih Meilani Yuliadi";
+  body.appendChild(pWm);
+}
